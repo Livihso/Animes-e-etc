7201e630d88de3348b02885aced61e25da2fe20c
Livihso <petrech.livia@escola.pr.gov.br>
2026-05-10 19:41:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,4 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('cadastro.html')
-
-usuario = input("Usuário: ")
-senha = input("Senha: ")
-
-if usuario == "Lívia" and senha == "quack":
-    print("Login realizado com sucesso")
-else:
-  print("Acesso Negado")
-
 app.run(debug=True)
